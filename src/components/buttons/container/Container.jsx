1c56553ab74f51020be87d666cc098b602a64a93
carlilu <carluppi99@gmail.com>
2023-02-20 12:57:12 +0100

    created branch and component Container

---
+++ src/components/buttons/container/Container.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { Container1 } from '@/components/container/containers-styled';
+
+export const Container = () => {
+  return (
+    <div> 
+        <Container1> Hola </Container1>
+    </div>
+  )
+}
---
+++ src/components/buttons/container/containers-styled.js
@@ -0,0 +1,12 @@
+import React from "react";
+import styled from "styled-components";
+// import {colorVariables}  from '@utils/variables.js';
+
+
+
+export const Container1 = styled.section`
+
+    /* display: flex;
+    flex-wrap: nowrap; */
+    
+`
