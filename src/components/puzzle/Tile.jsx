73444d99370662cb9952618bda1da077c0e73a43
fcegatti <fcegatti@yahoo.es>
2023-02-20 18:48:00 +0100

    Tile.js animations adapted to framer-motion

--- src/components/puzzle/Tile.jsx
+++ src/components/puzzle/Tile.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Motion, spring } from "react-motion";
+import { motion } from "framer-motion";
 import { getMatrixPosition, getVisualPosition } from "./helpers";
 import { TILE_COUNT, GRID_SIZE, BOARD_SIZE } from "./constants"
 
@@ -8,38 +8,33 @@ function Tile(props) {
   console.log("img in tile", imgUrl)
   const { row, col } = getMatrixPosition(index);
   const visualPos = getVisualPosition(row, col, width, height);
+  
   const tileStyle = {
     width: `calc(100% / ${GRID_SIZE})`,
     height: `calc(100% / ${GRID_SIZE})`,
-    translateX: visualPos.x,
-    translateY: visualPos.y,
+    transform: `translate3d(${visualPos.x}px, ${visualPos.y}px, 0)`,
     backgroundImage: `url(${imgUrl})`,
     backgroundSize: `${BOARD_SIZE}px`,
     backgroundPosition: `${(100 / (GRID_SIZE - 1)) * (tile % GRID_SIZE)}% ${(100 / (GRID_SIZE - 1)) * (Math.floor(tile / GRID_SIZE))}%`,
-
   };
-  const motionStyle = {
-    translateX: spring(visualPos.x),
-    translateY: spring(visualPos.y)
-  }
+
+  const transition = { type: "spring", stiffness: 250, damping: 20 };
 
   return (
-    <Motion style={motionStyle}>
-      {({ translateX, translateY }) => (
-        <li
-          style={{
-            ...tileStyle,
-            transform: `translate3d(${translateX}px, ${translateY}px, 0)`,
-            // Is last tile?
-            opacity: tile === TILE_COUNT - 1 ? 0 : 1,
-          }}
-          className="tile"
-          onClick={() => handleTileClick(index)}
-        >
-          {!imgUrl && `${tile + 1}`}
-        </li>
-      )}
-    </Motion>
+    <motion.li
+      style={tileStyle}
+      initial={false}
+      animate={{
+        x: visualPos.x,
+        y: visualPos.y,
+      opacity: tile === TILE_COUNT - 1 ? 0 : 1,
+      }}
+      transition={transition}
+      className="tile"
+      onClick={() => handleTileClick(index)}
+    >
+      {!imgUrl && `${tile + 1}`}
+    </motion.li>
   );
 }
 
