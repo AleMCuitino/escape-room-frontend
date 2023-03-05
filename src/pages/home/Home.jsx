28b9652ebd069541b94d01be06375af8dce36899
AleMCuitino <ale.morales11@gmail.com>
2023-03-05 16:53:34 +0100

    Fixed bug wordle game

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,15 +1,10 @@
 import React from 'react';
 import Wordle from '@/components/wordle/Wordle';
-import Puzzle from '@/components/puzzle/Puzzle';
-import Memorize from '@/components/memorize/Memorize';
 
 function Home() {
     return (
         <div>
-            <h2> Hola Escape-room </h2>
             <Wordle />
-            <Memorize />            
-            <Puzzle />
         </div>
     )
 }
