754b58ae85190c2150d94a31b95962fbb54e0b8d
fcegatti <fcegatti@yahoo.es>
2023-03-05 11:34:08 +0100

    spell check on stage-three db

--- src/pages/escape-room/stage-three/ENIAC_db.js
+++ src/pages/escape-room/stage-three/ENIAC_db.js
@@ -23,7 +23,7 @@ export const EniacDB =
       }],
       solution: "pasta",
       logicGame: {
-        text:"`Las programadoras de la máquina ENIAC han creado un algoritmo para controlar el flujo de energía en las entrañas de la máquina. El equipo cuenta con un plano de los circuitos  de la máquina que muestra la red de componentes que requieren suministro energético.  Para garantizar la eficiencia, la corriente debe llegar a todos los componentes sin pasar dos veces por el mismo y regresando al punto de origen. Cada componente está señalado con un letra ¿Podríais encontrar la sucesión de letras correcta? ingrésala en minúsculas y sin espacios` ",
+        text:"`Las programadoras de la máquina ENIAC han creado un algoritmo para controlar el flujo de energía en las entrañas de la máquina. El equipo cuenta con un plano de los circuitos  de la máquina que muestra la red de componentes que requieren suministro energético.  Para garantizar la eficiencia, la corriente debe llegar a todos los componentes sin pasar dos veces por el mismo y regresando al punto de origen. Cada componente está señalado con un letra ¿Podríais encontrar la sucesión de letras correcta? Ingrésala en minúsculas y sin espacios.` ",
         img:`${LogicGame}`,
         result: "arribalabirra"
       },
