a44d478af40657382c505f75cef53ca6efa9c628
Gary HL <garyhl321@gmail.com>
2023-03-06 08:59:57 +0100

    create new service register admin

--- src/services/user.service.jsx
+++ src/services/user.service.jsx
@@ -2,10 +2,25 @@ import axios from "axios";
 
 const baseUrl = import.meta.env.VITE_BACKEND_URL;
 
+const user = JSON.parse(localStorage.getItem('user')) ? JSON.parse(localStorage.getItem('user')) : '.';
+
+const token = user.token;
+
 export const login = (data) => {
     return axios.post(baseUrl + "/login", data);
 };
 
 export const register = (data) => {
     return axios.post(baseUrl + "/register", data);
+};
+
+export const register_admin = (data) => {
+
+    const config = {
+        headers: {
+            Authorization: `Bearer ${token}`,
+        },
+    };
+
+    return axios.post(baseUrl + "/register_admin", data, config);
 };
