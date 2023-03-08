28db126e75bb3381f84417227892c5441fa51ac6
carlilu <carluppi99@gmail.com>
2023-03-08 12:14:55 +0100

    changes in view start and add component chat

--- src/pages/escape-room/Start/Start.jsx
+++ src/pages/escape-room/Start/Start.jsx
@@ -1,38 +1,38 @@
 import React from 'react'
 import ButtonsF from '../../../components/buttons/ButtonsF'
-import { ContainerBody, ContainerButtons, Look, Text } from '../../../components/buttons/start-styled'
+import { ContainerBody, ContainerButtons, Look, Text} from '../../../components/buttons/start-styled'
 import Target from '@/assets/buttons/Target.svg';
-import { Link } from 'react-router-dom';
+import Chat from '../../../components/chat/Chat';
+
 
 const Start = () => {
 
-
-
-
   
   return (
-    <div>
+    <>
 
         <ContainerBody >
 
-          <div className='container vh-100 d-flex flex-column '>
+          <div className='container h-100 d-flex flex-column '>
 
-              <Text > 
-                  <h3>Comenzamos!</h3>
-                  <p> Hagan click en el botón correcto para corregir el eror 404 </p>
-              </Text>
-              
-              <ContainerButtons >            
-                  <ButtonsF > </ButtonsF>
-              </ContainerButtons>
-              <Look>
-              <img src={Target} alt="" />  
-              
-              </Look>
-              
+                <Text > 
+                    <h3>Comenzamos!</h3>
+                    <p> Hagan click en el botón correcto para corregir el eror 404 </p>
+                </Text>
+                
+                <ContainerButtons >            
+                    <ButtonsF > </ButtonsF>
+                </ContainerButtons>
+
+                <div className='col-12 '>
+                    <Chat/>
+                </div>
+
           </div>
         </ContainerBody>
-    </div>
+
+        
+    </>
   )
 }
 
