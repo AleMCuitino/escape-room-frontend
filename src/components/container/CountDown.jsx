366d760434bd08e274e881d784f96950352fcc31
Gary HL <garyhl321@gmail.com>
2023-03-08 18:52:16 +0100

    add usenavigate2

--- src/components/container/CountDown.jsx
+++ src/components/container/CountDown.jsx
@@ -1,43 +1,51 @@
 import React, { useState, useRef, useEffect } from 'react'
+import { useNavigate } from 'react-router';
+import { useLocalStorage } from '../../hooks/useLocalStorage';
 
 
 const CountDown = () => {
+    
+    const navigate = useNavigate();
 
-   // We need ref in this, because we are dealing
-    // with JS setInterval to keep track of it and
-    // stop it when needed
     const Ref = useRef(null);
 
+    const [timeLeft, setTimeLeft] = useLocalStorage('timeLeft', '');
+
     const [timer, setTimer] = useState('00:00');
-    
+
     const getTimeRemaining = (e) => {
         const total = Date.parse(e) - Date.parse(new Date());
         const seconds = Math.floor((total / 1000) % 60);
         const minutes = Math.floor((total / 1000 / 60) % 60);
-        
+
         return {
             total, minutes, seconds
         };
     }
 
     const startTimer = (e) => {
-        let { total, minutes, seconds } 
-                    = getTimeRemaining(e);
+        let { total, minutes, seconds }
+            = getTimeRemaining(e);
         if (total >= 0) {
 
             // update the timer
             // check if less than 10 then we need to 
             // add '0' at the beginning of the variable
             setTimer(
-                
                 (minutes > 9 ? minutes : '0' + minutes) + ':'
                 + (seconds > 9 ? seconds : '0' + seconds)
-            )
+                )
+
         }
     }
-
+    useEffect(() => {
+        if(timer == '00:00'){
+            navigate('/final')
+        }
+    }, [timer])
+    
     const clearTimer = (e) => {
-        
+
         setTimer('59:59');
 
         // If you try to remove this line the 
@@ -46,17 +54,23 @@ const CountDown = () => {
         if (Ref.current) clearInterval(Ref.current);
         const id = setInterval(() => {
             startTimer(e);
+            
         }, 1000)
         Ref.current = id;
     }
 
     const getDeadTime = () => {
-        let deadline = new Date();
-
-        // This is where you need to adjust if 
-        // you entend to add more time
-        deadline.setSeconds(deadline.getSeconds() + 3600);
-        return deadline;
+        if (timeLeft) {
+            let deadline = new Date(timeLeft);
+            deadline.setSeconds(deadline.getSeconds() + 3600);
+            return deadline;
+        }
+        else{
+            let deadline = new Date();
+            setTimeLeft(deadline);
+            deadline.setSeconds(deadline.getSeconds() + 3600);
+            return deadline;
+        }
     }
 
     // We can use useEffect so that when the component
@@ -67,11 +81,12 @@ const CountDown = () => {
 
     useEffect(() => {
         clearTimer(getDeadTime());
+        
     }, []);
 
     return (
         <div className='text-end mb-4 mb-md-0'>
-            <h2>{timer}</h2>  
+            <h2>{timer}</h2>
         </div>
     )
 }
